0bbbb1a0a685626aae409ca8b3919f28423c2340
Edwin Lamanna <edlamanna@hotmail.com>
2024-09-03 17:52:41 +0200

    corrige los iconos de carrito y favoritos en store.jsx

--- frontend/src/components/Store.jsx
+++ frontend/src/components/Store.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { ProductContext } from '../context/ProductContext.jsx'
-import { Link } from 'react-router-dom'
+import CardProducts from '../components/CardProducts.jsx'
 
 const Store = ({ category }) => {
   const { products, fetchProducts, loading, error } = useContext(ProductContext)
@@ -25,26 +25,8 @@ const Store = ({ category }) => {
             {products.length > 0
               ? (
                   products.map((product) => (
-                    <div key={`${product.id}-${product.talla}`} className='card'>
-                      <Link to={`/tienda/${product.id}`} className='link-to-tienda'>
-                        <div className='card__header'>
-                          <div className='card__header-item'>
-                            <img
-                              className='card__header-img'
-                              src={product.image_urls[0]} // Mostrar la primera imagen
-                              alt={product.nombre}
-                            />
-                          </div>
-                          <div className='card__price'>
-                            <h2 className='card__price-title'>
-                              <b>$ {product.precio_unitario.toLocaleString()}</b>
-                            </h2>
-                          </div>
-                        </div>
-                        <div className='card__body'>
-                          <h2 className='card__body-title'>{product.nombre}</h2>
-                        </div>
-                      </Link>
+                    <div key={`${product.id}-${product.talla}`} className='col-12 col-md-4 mb-4'>
+                      <CardProducts product={product} />
                     </div>
                   ))
                 )
